647fd4c73c17bd1fae1964d5f8f39946c4c06a4d
RouSage <therousage@gmail.com>
2020-02-20 17:50:04 +0300

    Controller to server OIDC parameters to client

---
+++ src/BlogAppCore/Controllers/OidcConfigurationController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BlogAppCore.Controllers
+{
+    public class OidcConfigurationController : Controller
+    {
+        public OidcConfigurationController(
+            IClientRequestParametersProvider clientRequestParametersProvider,
+            ILogger<OidcConfigurationController> logger)
+        {
+            ClientRequestParametersProvider = clientRequestParametersProvider;
+        }
+
+        public IClientRequestParametersProvider ClientRequestParametersProvider { get; }
+
+        [HttpGet("_configuration/{clientId}")]
+        public IActionResult GetClientRequestParameters([FromRoute]string clientId)
+        {
+            var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
+
+            return Ok(parameters);
+        }
+    }
+}
